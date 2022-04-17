de978d197ceafc051f3170548831fe6af0ca316a
UlfetZakirli <ulfetzakirli@gmail.com>
2022-04-17 16:22:20 +0400

    myReactForm

---
+++ setup/src/data.js
@@ -0,0 +1,34 @@
+const questions = [
+  {
+    id: 1,
+    title: 'What will you learn in Compar?',
+    info:
+      'Full-stack(frontEnd and backEnd)',
+  },
+  {
+    id: 2,
+    title: 'What kind of programing languages will you learn in frontEnd?',
+    info:
+      'HTML, CSS(SCSS/SASS), JavaScript, React and libraries like:(Bootstrap, Node.js, jQuery ), Git/GitHub',
+  },
+  {
+    id: 3,
+    title: 'What kind of programing languages will you learn in backEnd?',
+    info:
+      'C#(.NET Core, ASP.NET Core, MVC, Forms, API), MsSQL, Phyton etc.',
+  },
+  {
+    id: 4,
+    title: 'How long is taught?',
+    info:
+      '5 months front and 5 months back. But you know, actually it dependes on people',
+      
+  },
+  {
+    id: 5,
+    title: 'where can you find us?',
+    info:
+      'Nizami metro station Zergerpalan street 137, Baku Azerbaijan',
+  },
+]
+export default questions
